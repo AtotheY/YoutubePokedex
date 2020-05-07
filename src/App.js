3c2aca3a5036246aef35f811b220cff1015c218e
AtotheY <sistillianthony@gmail.com>
2020-05-07 14:03:54 -0400

    get rid of useless div

--- src/App.js
+++ src/App.js
@@ -4,16 +4,14 @@ import Pokemon from "./Pokemon";
 import { Route, Switch } from "react-router-dom";
 
 const App = () => (
-  <div>
-    <Switch>
-      <Route exact path="/" render={(props) => <Pokedex {...props} />} />
-      <Route
-        exact
-        path="/:pokemonId"
-        render={(props) => <Pokemon {...props} />}
-      />
-    </Switch>
-  </div>
+  <Switch>
+    <Route exact path="/" render={(props) => <Pokedex {...props} />} />
+    <Route
+      exact
+      path="/:pokemonId"
+      render={(props) => <Pokemon {...props} />}
+    />
+  </Switch>
 );
 
 export default App;
